60c2632a9efb5523e14750d8aab1a92caef681a4
Brandon Herlong <49008607+BrandonLHerlong@users.noreply.github.com>
2021-07-15 12:06:06 -0400

    Update main.py
    Remove secret key from public

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogz@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-app.secret_key = '5003094'
+app.secret_key = ''
 
 
 
